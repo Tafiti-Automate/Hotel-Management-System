c7726531823866d016aa2dd2ec41dcc2c4802d3c
Watumwa <watumwaizaac32@gmail.com>
2026-06-24 15:23:03 +0300

    hotel profile

--- hotel_erp_backend/core/urls.py
+++ hotel_erp_backend/core/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import include, path
 from django.views.generic import RedirectView
 
@@ -32,3 +34,6 @@ urlpatterns = [
     path("api/v1/health/", health_check, name="health-check"),
     path("api/v1/", include(api_patterns)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
