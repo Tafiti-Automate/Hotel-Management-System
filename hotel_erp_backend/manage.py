9655c8ebfe7652a8ba11602e5d82bb90facb2212
Watumwa <watumwaizaac32@gmail.com>
2026-07-29 14:38:57 +0300

    fixed app.py 404 error

--- hotel_erp_backend/manage.py
+++ hotel_erp_backend/manage.py
@@ -5,7 +5,12 @@ import sys
 
 
 def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.dev")
+    default_settings = (
+        "core.settings.prod"
+        if os.environ.get("VERCEL")
+        else "core.settings.dev"
+    )
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", default_settings)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
